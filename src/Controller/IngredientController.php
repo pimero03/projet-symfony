dfa02f57f6b87ba369dc9f1b2b8401f02f030b7d
Melien Pierre <mrolpierre@gmail.com>
2021-11-04 14:27:22 +0100

    fist commit

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/ingredient')]
+class IngredientController extends AbstractController
+{
+    #[Route('/', name: 'ingredient_index', methods: ['GET'])]
+    public function index(IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('ingredient/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'ingredient_new', methods: ['GET','POST'])]
+    public function new(Request $request, SluggerInterface $slugger): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $file = $form->get('file')->getData();
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $file->move(
+                        $this->getParameter('files_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $ingredient->getImageName($newFilename);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ingredient/new.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'ingredient_show', methods: ['GET'])]
+    public function show(Ingredient $ingredient): Response
+    {
+        return $this->render('ingredient/show.html.twig', [
+            'ingredient' => $ingredient,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'ingredient_edit', methods: ['GET','POST'])]
+    public function edit(Request $request, Ingredient $ingredient): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'ingredient_delete', methods: ['POST'])]
+    public function delete(Request $request, Ingredient $ingredient): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ingredient->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($ingredient);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('ingredient_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/IngredientType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\File;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('type',ChoiceType::class, [
+                'choices'  => [
+                    'liquide' => 'liquide',
+                    'solde' => 'solde',
+                    'piece' => 'piece',
+                ],
+            ])
+            ->add('file', FileType::class, [
+                'label' => 'Image de l\'ingrédient',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/*',
+                        ],
+                        'mimeTypesMessage' => 'Entrez une image valide',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
