623068df75dd77d1acb5b3deb8bd1e9a360e5d13
Melien Pierre <mrolpierre@gmail.com>
2021-11-05 10:57:50 +0100

    wip

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/')]
+#[Route('/recette')]
 class RecetteController extends AbstractController
 {
     #[Route('/', name: 'recette_index', methods: ['GET'])]
